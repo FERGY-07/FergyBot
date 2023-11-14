db631e2b57b05a270d8c7961ecedae3872e20b64
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2023-11-13 22:31:34 -0500

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n┃ 💎 /support`
+		let bio = `\n🟢 MichiBot Tiempo Activo ${muptime}\n\n┃ 💎 /support`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
